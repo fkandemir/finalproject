dcd4ce65b78d433f2f97040ad037d04b296e69e7
Faruk Kandemir <ffkandemir@gmail.com>
2024-04-28 20:35:45 +0300

    Client portal updates
    Client portal features:  Index page, list cars by brand or list all cars. Search cars by brand, model, year and price. About us page.

--- client_portal/views.py
+++ client_portal/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from employee_portal.models import Car
 from django.db.models import Count
+from django.http import JsonResponse
+import random
+from django.db.models import Q  # Import Q for complex queries
 # Create your views here.
 
+
+
 def index(request):
-    return render(request, "client_portal/generic_index.html")
+    # Fetch the latest three cars based on their IDs
+    latest_cars = Car.objects.order_by('-id')[:3]
+    return render(request, "client_portal/generic_index.html", {'latest_cars': latest_cars})
+
 
 def allCars(request):
     cars = Car.objects.all()  # Fetch all car objects from the database
@@ -21,4 +29,24 @@ def cars_by_brand(request, brand_name):
 def carBrandsView(request):
     brands = Car.objects.values('brand').annotate(count=Count('brand')).order_by('brand')
     return render(request, 'client_portal/carBrands.html', {'brands': brands})
+
+def searchCarView(request):
+    query = request.GET.get('query', '')
+    if query:
+        cars = Car.objects.filter(
+            Q(brand__icontains=query) |
+            Q(model__icontains=query) |
+            Q(price__icontains=query) |
+            Q(model_year__icontains=query)
+        )
+    else:
+        cars = Car.objects.none()
+
+    return render(request, 'client_portal/search.html', {'cars': cars, 'request': request})
+
+#def contactView(request):
+#    return render(request, "client_portal/client_contact.html")
+
+def aboutusView(request):
+    return render(request, "client_portal/aboutUs.html")
 
